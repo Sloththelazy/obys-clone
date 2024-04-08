b991b97d50f9c75092665d32d417d634d4e5755e
JeevantPrakharSingh <jeevantprakharsingh2004@gmail.com>
2024-04-09 01:46:25 +0530

    Completed some animations and some other page realted issue are solved in page4 code as well as loader was made a little better

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function loadingAnimation() {
-    var tl = gsap.timeline();
-    
+var tl = gsap.timeline();
 tl.from (".line h1 , .line-h2 " , {
     y:150 ,
     stagger: 0.35,
@@ -46,6 +45,16 @@ tl.from("#page1" , {
 tl.to ("#loader" , {
     display: "none"
 })
+tl.from("#nav" , {
+    opacity:0,
+    ease:'power2.out'
+
+})
+tl.from("#hero1 h1 ,#hero2 h1 ,#hero3 h2,#hero4 h1", {
+    y:150 ,
+    stagger: 0.1,
+    ease:'power2.out'
+})
 
 // disabling the cursor for the loader 
 const yourDiv = document.getElementById("loader");
@@ -59,6 +68,59 @@ yourDiv.addEventListener('mouseleave', () => {
 }
 
 function cursorAnime() {
+    //Cumbunto Animation
+    var frames = document.querySelectorAll(".frame") 
+    const lerp = (x , y, a) => x * ( 1 - a ) + y * a ;
+
+    frames.forEach(frame => {
+        frame.addEventListener("mousemove" , function (dets) {
+
+            var dims = frame.getBoundingClientRect(); // gives you the dimensions of the frame
+            var xstart = dims.x
+            var xend = dims.x + dims.width
+            var zerotoone = gsap.utils.mapRange(xstart , xend , 0, 1, dets.clientX)
+           
+    
+            gsap.to("#crsr" , {
+                scale: 2 ,
+                innerHTML:"" ,
+                ease:'power4.out'
+            })
+            gsap.to(frame.children , {
+                color: "#fff" ,
+                fontSize: '1.5vw',
+                ease: "power4.out",
+                duration: 1, 
+                y: "-3.5vw"
+            })
+            gsap.to(frame.children , {
+                x: lerp(-75 , 75 , zerotoone),
+                duration: 0.3 ,
+                ease:"power4.out"
+            })
+        })
+        frame.addEventListener("mouseleave" , function(dets) {
+            gsap.to("#crsr" , {
+                scale:1 ,
+                innerHTML:"Scroll",
+                ease:"power4.out"
+            })
+            gsap.to(frame.children, {
+                color: "#fff" ,
+                fontSize: '1vw',
+                ease: "power4.out",
+                duration: 1,
+                y:0
+            })
+            gsap.to(frame.children , {
+                x: 0,
+                duration: 0.3 ,
+                ease:"power4.out"
+            })
+        })
+    })
+   
+
     document.addEventListener("mousemove", function(dets) {
         gsap.to("#crsr", {
             left: dets.x,
@@ -66,18 +128,55 @@ function cursorAnime() {
         });
     });
     document.addEventListener("mouseenter" , function() {
-        gsap.to("#crsr" , {
+        gsap.to("#crsr" , { // mouse enter and leave animation
             scale: 1 ,
             opacity: 1
         })
     })
     document.addEventListener("mouseleave" , function() {
-        gsap.to("#crsr" , {
+        gsap.to("#crsr" , { // mouse leave animation (this is a problem in the website the mosue seems stuck when we leave the window)
             scale: 0,
             opacity:0
         })
     })
 }
+ function imageAnime() {
+    var Elem = document.querySelectorAll(".blue-div-elem"); // gives us a node list
+    var imgElem = document.querySelectorAll(".blue-div-elem img");
 
-cursorAnime()
+    Elem.forEach(function(elem){
+        elem.addEventListener("mouseenter" , function(){
+            // console.log(elem.childNodes) i used this to find the index of image in the nodelist
+            
+            gsap.to(elem.childNodes[5] , {
+                opacity: 1,
+                scale:1 ,
+                ease: 'power2.out'
+            })
+        })
+        elem.addEventListener("mouseleave" , function () {
+            // console.log(elem.childNodes)
+            gsap.to(elem.childNodes[5] , {
+                opacity: 0,
+                scale:0 ,
+                ease:'power2.out'
+            })
+        })
+        // dets ---- gives us the details about the x and the y axis coordinates of the mouse
+        // in real time and we can use it to make a animation for when the mouse enters a
+        // particular div
+        elem.addEventListener("mousemove" , function(dets) {
+            // since div is also used for making shapes hence we have properties to know about is 
+            // height and width one such property can tell us from where to where hsall the image
+            // follow the cursor
+            gsap.to(elem.childNodes[5] , {
+                x:dets.x - elem.getBoundingClientRect().x -25 ,
+                y:dets.y - elem.getBoundingClientRect().y -50  
+            })
+        })
+    })
+ }
+
+ imageAnime();
+cursorAnime();
 loadingAnimation();
