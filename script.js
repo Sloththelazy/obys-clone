407291b684b0b6c65e039227b4048c5995884c3f
JeevantPrakharSingh <jeevantprakharsingh2004@gmail.com>
2024-04-11 04:10:02 +0530

    Page 3 left and then js animations will begin

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+  
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true,
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+  
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main").style.transform
+        ? "transform"
+        : "fixed",
+    });
+  
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+  }
+
 function loadingAnimation() {
 var tl = gsap.timeline();
 tl.from (".line h1 , .line-h2 " , {
@@ -5,7 +45,7 @@ tl.from (".line h1 , .line-h2 " , {
     stagger: 0.35,
     duration: 0.8,
     ease:'power2.out',
-    delay: 0.4
+    delay: 0.35
 })
 tl.from ("#line1-part1  " , {
     opacity: 0,
@@ -23,12 +63,14 @@ tl.from ("#line1-part1  " , {
     },
 });
 
-// tl.to(" .line h2" , {
-//     animationName: "anime" ,
-// })
+tl.to(".line-h2", {
+    animationName: "loaderAnime",
+    opacity: 1,
+    ease:'power4.out'
+  });
 tl.from (".lineh3" , {
     opacity: 0 ,
-    ease: Power4, 
+    ease: 'power2.out', 
 });
 tl.to ("#loader" , {
     opacity: 0,
@@ -40,7 +82,7 @@ tl.from("#page1" , {
     delay:0.2,
     y:1600,
     opacity:0,
-    ease:Power4
+    ease:'power2.out'
 });
 tl.to ("#loader" , {
     display: "none"
@@ -76,8 +118,8 @@ function cursorAnime() {
         frame.addEventListener("mousemove" , function (dets) {
 
             var dims = frame.getBoundingClientRect(); // gives you the dimensions of the frame
-            var xstart = dims.x
-            var xend = dims.x + dims.width
+            var xstart = dims.x - 10
+            var xend = dims.x + dims.width -10
             var zerotoone = gsap.utils.mapRange(xstart , xend , 0, 1, dets.clientX)
            
     
@@ -139,6 +181,20 @@ function cursorAnime() {
             opacity:0
         })
     })
+
+    var page4 = document.querySelector("#page4")
+    page4.addEventListener("mouseenter" , function() {
+        gsap.to("#crsr" , { // mouse enter and leave animation
+            scale: 0 ,
+            opacity: 0
+        })
+    })
+    page4.addEventListener("mouseleave" , function() {
+        gsap.to("#crsr" , { // mouse leave animation (this is a problem in the website the mosue seems stuck when we leave the window)
+            scale: 1,
+            opacity:1
+        })
+    })
 }
  function imageAnime() {
     var Elem = document.querySelectorAll(".blue-div-elem"); // gives us a node list
@@ -176,7 +232,7 @@ function cursorAnime() {
         })
     })
  }
-
- imageAnime();
+locomotiveAnimation();
+imageAnime();
 cursorAnime();
 loadingAnimation();
