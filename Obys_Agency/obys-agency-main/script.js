a11b8db4c566b5ef6261b37042db66a203e4a54b
JeevantPrakharSingh <jeevantprakharsingh2004@gmail.com>
2024-04-15 18:33:49 +0530

    Frontend Complete

--- Obys_Agency/obys-agency-main/script.js
+++ Obys_Agency/obys-agency-main/script.js
@@ -40,8 +40,8 @@ function locomotiveAnimation() {
 function loadingAnimation() {
   var tl = gsap.timeline();
   tl.from(".line h1", {
-    y: 170,
-    stagger: 0.25,
+    y: 200,
+    stagger: 0.35,
     duration: 0.6,
     delay: 0.5,
   });
@@ -56,20 +56,25 @@ function loadingAnimation() {
         } else {
           h5timer.innerHTML = grow;
         }
-      }, 27);
+      }, 16);
     },
   });
-  tl.to(".line h2", {
+  tl.to(".line-h2", {
     animationName: "loaderAnime",
     opacity: 1,
+    ease:'power4.out'
   });
+  tl.from (".lineh3" , {
+    opacity: 0 ,
+    ease: 'power2.out', 
+});
   tl.to("#loader", {
     opacity: 0,
     duration: 0.2,
     delay: 2.6,
   });
   tl.from("#page1", {
-    delay: 0.1,
+    delay: 0.2,
     y: 1600,
     duration: 0.5,
     ease: Power4,
@@ -78,11 +83,14 @@ function loadingAnimation() {
     display: "none",
   });
   tl.from("#nav", {
-    opacity: 0,
+    y:140,
+    opacity:0,
+    ease:'power2.out'
   });
   tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
     y: 140,
     stagger: 0.2,
+    ease:'power4.out'
   });
   tl.from(
     "#hero1, #page2",
@@ -101,8 +109,8 @@ function cursorAnimation() {
       frame.addEventListener("mousemove" , function (dets) {
 
           var dims = frame.getBoundingClientRect(); // gives you the dimensions of the frame
-          var xstart = dims.x
-          var xend = dims.x + dims.width
+          var xstart = dims.x - 10
+          var xend = dims.x + dims.width -10
           var zerotoone = gsap.utils.mapRange(xstart , xend , 0, 1, dets.clientX)
          
   
@@ -165,6 +173,35 @@ function cursorAnimation() {
       })
   })
 
+  var page4 = document.querySelector("#page4")
+  var page3 = document.querySelector("#page3")
+
+  page3.addEventListener("mouseenter" , function() {
+      gsap.to("#crsr" , { // mouse enter and leave animation
+          scale: 0 ,
+          opacity: 0
+      })
+  })
+  page3.addEventListener("mouseleave" , function() {
+      gsap.to("#crsr" , { // mouse leave animation (this is a problem in the website the mosue seems stuck when we leave the window)
+          scale: 1,
+          opacity:1
+      })
+  })
+
+
+  page4.addEventListener("mouseenter" , function() {
+      gsap.to("#crsr" , { // mouse enter and leave animation
+          scale: 0 ,
+          opacity: 0
+      })
+  })
+  page4.addEventListener("mouseleave" , function() {
+      gsap.to("#crsr" , { // mouse leave animation (this is a problem in the website the mosue seems stuck when we leave the window)
+          scale: 1,
+          opacity:1
+      })
+  })
 
 
 
@@ -189,13 +226,13 @@ function cursorAnimation() {
     }
   })
 }
-function sheryAnimation() {
-  Shery.imageEffect(".image-div", {
-    style: 5,
-    gooey: true,
-    // debug:true,
-    config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7241195453907675},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.23,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.5,"range":[0,10]},"metaball":{"value":0.33,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.01,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}
-  });
+function sheryAnimation(){
+  Shery.imageEffect(".image-div" , {
+      style:5,
+      // debug:true,
+      config:{"a":{"value":0.45,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.6857083080101598},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.2,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":1.15,"range":[0,10]},"metaball":{"value":0.52,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+      gooey:true
+  })
 }
 function flagAnimation() {
 
